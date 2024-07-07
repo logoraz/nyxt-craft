0b71d188416d325fcc03e7fb7d8402a962c8aa9a
logoraz <erikalmaraz@fastmail.com>
2024-07-06 20:10:37 -0700

    Clean up config, add adhock gnome support for custom nyxt builds"

--- gnome/start-nyxt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Setup for GI-GTK Nyxt to look descent in Gnome (on Guix)
-
-# Set title bar of GTK applications 
-export GTK_THEME=Adwaita:dark
-
-# Launch Nyxt in the background
-/home/logoraz/common-lisp/nyxt/nyxt &
