2c92fdf9b020e4c5c85dbdf3cd4594a7ed231f36
logoraz <erikalmaraz@fastmail.com>
2024-07-09 13:59:30 -0700

    Refacotor password config.

--- passwords.lisp
+++ passwords.lisp
@@ -15,15 +15,15 @@
 (defmethod initialize-instance :after ((interface password:keepassxc-interface)
                                        &key &allow-other-keys)
   (setf (password:password-file interface) "/home/logoraz/Documents/moses/p.kdbx"
-        (password:key-file interface) "/home/logoraz/Documents/moses/pkf"
-        (password:yubikey-slot interface) nil))
+        (password:key-file interface) "/home/logoraz/Documents/moses/pkf"))
 
 (define-configuration nyxt/mode/password:password-mode
   ((password-interface (make-instance 'password:keepassxc-interface))))
 
 ;; https://github.com/atlas-engineer/nyxt/issues/3213
+;; FIXME -> Fails, see how to get this working in the interim while issue is addressed
 ;; (define-configuration password:keepassxc-interface
 ;;   ((password:executable (str:split " " "flatpak-spawn --host keepassxc-cli"))))
 
 (define-configuration buffer
-    ((default-modes `(password-mode ,@%slot-value%))))
+  ((default-modes `(password-mode ,@%slot-value%))))
