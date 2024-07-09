dca6da9e79ceec8af028fae91565a115cb3dc073
logoraz <erikalmaraz@fastmail.com>
2024-07-09 13:59:16 -0700

    Refactor main config.

--- config.lisp
+++ config.lisp
@@ -13,28 +13,37 @@
 
 (in-package #:nyxt-user)
 
-;;; Nyxt custom modifications
-;; Loading files from the same directory (~/.config/nyxt/).
+;;; Reset ASDF registries to allow loading Lisp systems from
+;;; everywhere.
+;; #+nyxt-3 (reset-asdf-registries)
+
+;;; Load quicklisp
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+;;; TODO - perhaps set for a specialized class
+(defvar *buffer-modes*
+  '(emacs-mode)
+  "Mode to enable in parent class `buffer' to apply to all child buffer classes.")
+
+;;; Loading files from the same directory (~/.config/nyxt/).
 (define-nyxt-user-system-and-load nyxt-user/basic-config
   :components ("theme"
                "passwords"
                ;; "passwords-dev"
                "utilities"))
 
-;;; Base broswer/buffer configurations
+;;; Don't restore previous session for now
 (define-configuration browser
   ((restore-session-on-startup-p nil)))
 
+;;;
 (define-configuration buffer
-    ((default-modes `(emacs-mode ,@%slot-value%))))
+  ((default-modes `(,@*buffer-modes* ,@%slot-value%))))
 
-;; Keeping for reference, prefer bookmarks to be kept in default location: ~/.local/share/nyxt/
-;; (defmethod files:resolve ((profile nyxt:nyxt-profile) (file nyxt/mode/bookmark:bookmarks-file))
-;;   "Reroute bookmarks to the `.config/nyxt/' directory."
-;;   #p"~/Documents/moses/bookmarks.lisp")
-
-;;; Load External Packages/Libraries
-;; (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-;;                                        (user-homedir-pathname))))
-;;   (when (probe-file quicklisp-init)
-;;     (load quicklisp-init)))
+(defmethod files:resolve ((profile nyxt:nyxt-profile) (file nyxt/mode/bookmark:bookmarks-file))
+  "Reroute bookmarks to the `.config/nyxt/' directory."
+  #p"~/.config/nyxt/bookmarks.lisp")
